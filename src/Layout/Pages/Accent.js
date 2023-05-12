d7ea3a283dfefcdfbb9d4ce265f44a0f7707d7ea
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-05-11 19:23:34 -0600

    Update Accent.js

--- src/Layout/Pages/Accent.js
+++ src/Layout/Pages/Accent.js
@@ -23,7 +23,7 @@ export default function Accent() {
       </div>
     </div>
 <div className="bg-white py-24 px-10 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-4">
             <div className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
@@ -60,7 +60,7 @@ export default function Accent() {
       <div className="my-36">
         <Splider />
       </div>
-      <div className="mt-20 ml-10 mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div className="mt-20 lg:ml-10 mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div>
             <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
               
