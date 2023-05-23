5d02f8dcbd954709b103cc8aabe1a67297b7cfde
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-05-23 12:42:00 -0600

    Update Splider.js

--- src/Layout/Pages/Splider.js
+++ src/Layout/Pages/Splider.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/css';
 import accent from "../../Componets/light pictures/accent.JPG"
-import red from "../../Componets/light pictures/red.JPG"
+import green from "../../Componets/light pictures/green.JPG"
 import usa from "../../Componets/light pictures/usa.JPG"
 import easter from "../../Componets/light pictures/easter.JPG"
 import christmas from "../../Componets/light pictures/christmas.JPG"
@@ -11,32 +11,37 @@ import valentines from "../../Componets/light pictures/valentines.JPG"
 export default function Splider() {
     
     return(
-        <div>
+        <div className="bg-slate-900">
         <Splide aria-label="My Favorite Images" className=" md:px-32  md:space-x-10" options={ {
           perMove : 1,
-          perPage : 1,
+          perPage : 2,
           rewind: true,
-          pagination: false,
-          
+          pagination: true,
+          gap: 40,
+          breakpoints: {
+            740: { perPage : 1,
+            gap: 0,
+          },
+          }
         } }>
           <SplideSlide>
-            <img src={usa} alt="Image 1" className="h-auto w-auto shadow-xl rounded-3xl"
+            <img src={usa} alt="1" className="h-auto w-auto shadow-xl rounded-3xl"
             />
           </SplideSlide>
           <SplideSlide>
-            <img src={accent} alt="Image 2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={accent} alt="2" className="h-auto w-auto shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={red} alt="Image 2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={green} alt="3" className="h-auto w-auto shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={valentines} alt="Image 2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={valentines} alt="4" className="h-auto w-auto shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={easter} alt="Image 2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={easter} alt="5" className="h-auto w-auto shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={christmas} alt="Image 2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={christmas} alt="6" className="h-auto w-auto shadow-xl rounded-3xl"/>
           </SplideSlide>
         </Splide>
         </div>
