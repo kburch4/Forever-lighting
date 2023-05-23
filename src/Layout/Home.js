4093184b72a59cc6b7750c59301ffb2fb589facb
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-05-23 12:27:28 -0600

    Update Home.js
    updated dark home

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,7 +1,5 @@
 import React from "react"
-import accent from "../Componets/light pictures/accent.JPG"
 import darkblue from "../Componets/light pictures/blue.JPG"
-import bronco from "../Componets/light pictures/bronco.jpg"
 import day from "../Componets/light pictures/day.JPG"
 import './Home.css';
 import { SunIcon, GiftIcon, DevicePhoneMobileIcon, LockClosedIcon } from '@heroicons/react/24/outline'
@@ -10,6 +8,10 @@ import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
 import Quote from "./Pages/Quote"
 import Splider from "./Pages/Splider"
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
+import phone from "../Componets/light pictures/phone.jpg"
+import valentines from "../Componets/light pictures/valentines.JPG"
+import multi2 from "../Componets/light pictures/multi2.jpg"
+import vid from "../Componets/light pictures/webvid.mp4"
 
 const faqs = [
   {
@@ -66,61 +68,166 @@ const features = [
 ]
 const stats = [
     { id: 1, name: 'Color combinations', value: '+1,000,000' },
-    { id: 2, name: 'preset combinations', value: '100' },
-    { id: 3, name: 'Different color sequences', value: '500' },
+    { id: 2, name: 'Light Effects', value: '+100' },
+    { id: 3, name: 'Designs', value: 'Unlimited' },
   ]
    
   
   export default function Home() {
     
     return (
-        <><div className="relative overflow-hidden bg-white">
-            <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
-                <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-                    <div className="sm:max-w-lg -mt-20">
-                    <h1 className="pt-10 pb-15 text-5xl text-black font-extrabold font-teko">Forever <span className="text-sky-600">Lighting</span></h1>
+        <>
+        <div className="bg-slate-900">
+        <div className="relative isolate overflow-hidden pt-14 bg-slate-800">
+        <video 
+          autoplay="{true}"
+          muted 
+          loop
+          className="absolute inset-0 -z-10 h-full w-full object-cover mix-blend-multiply bg-video">
+          <source 
+          src={vid}
+          type="video/mp4"
+          />
+        </video>
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+        </div>
+        <div className="mx-auto max-w-2xl py-24 sm:py-48 lg:py-32">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-sky-400 ring-1 bg-sky-400/10 ring-sky-400/10 hover:ring-sky-400/20 hover:bg-sky-400/20">
+              Learn more about our LED Smart Lights {' '}
+              <a href="#" className="font-semibold text-sky-400">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+          <h1 className="pt-10 pb-15 text-5xl md:text-7xl text-white font-extrabold font-teko">Forever <span className="text-sky-600">Lighting</span></h1>
                     
-                        <p className="mt-10 text-xl text-gray-500">
-                            Never put lights back on with Forever Lighting and celebrate every event all year.
-                        </p>
-                        <div>
-                            <button 
-                            href="/Quote"
-                            className="inline-block rounded-md border border-transparent bg-sky-600 py-3 px-8 text-center font-medium text-white hover:bg-sky-700 mt-10"
-                            >
-                            Request Free Quote
-                            </button>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="mt-10 pd-6">
-                            {/* Decorative image grid */}
-                            <div
-                                aria-hidden="true"
-                                className="pointer-events-none lg:absolute lg:inset-y-0 -left-20 lg:mx-auto lg:w-full lg:max-w-7x1"
-                            >
-                              
-                                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                                    <div className="flex items-center space-x-6 lg:space-x-8 h-auto w-auto">
-                                        
-                                                <img
-                                                    src={darkblue}
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center shadow-xl rounded-3xl" />
-                                            
-                                    </div>
-                                </div>
-                            </div>
-
-                            
-                        </div>
-                    </div>
-                </div>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+            Finally, there’s a better way to light up your house than hanging new lights for every season and event. 
+            Never put holiday lights on again. <span className="font-2xl">Ever.</span> 
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-sky-400/10 px-3.5 py-2.5 text-sm font-semibold text-sky-400 shadow-sm hover:bg-sky-400/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              >
+                Request Quote
+              </a>
+              
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          
+        </div>
+      </div>
+    </div>
+    <div className="bg-slate-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-4 rounded-lg">
+            
+            <ReactCompareSlider
+              itemOne={<ReactCompareSliderImage src={day} alt="Image one" className="rounded-3xl" />}
+              itemTwo={<ReactCompareSliderImage src={darkblue} alt="Image two" className="rounded-3xl"/>}
+              
+            />
+      
+            </div>
+          
+        <div>
+            <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
+              
+              <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Bright at Night and Invisible by Day
+              </h1>
+              <div className="max-w-xl">
+                <p className="mt-6 text-slate-400">
+                The lights are installed underneath the house’s roofline, making them unnoticeable from the street during the day. To make them even more invisible, we offer color matching so the light’s casing will match the trim piece of your house, giving them the ultimate disguise.
+                </p>
+              </div>
+              <div className="mt-12 hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-sky-400 ring-1 bg-sky-400/10 ring-sky-400/10 hover:ring-sky-400/20 hover:bg-sky-400/20">
+              Learn more about our LED Smart Lights {' '}
+              <a href="/Quote" className="font-semibold text-sky-400">
+                <span className="absolute inset-0" aria-hidden="true" />
+                <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+            
             </div>
             </div>
-          <div className="relative isolate overflow-hidden bg-gray-800 py-24 sm:py-32">
+           </div>
+          </div>
+         </div>
+         <div className="relative isolate pt-14 overflow-hidden bg-gray-700">
+        <img
+          src={valentines}
+          alt=""
+          className="absolute inset-0 mix-blend-multiply -z-10 h-full w-full object-cover"
+        />
+       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
+      <div className="relative mx-auto max-w-7xl px-6 lg:px-8">          
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Touch of a Button
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-slate-400">
+          Use our app to change to any colors, set timers, and even create a new sequence.
+          </p>
+        </div>
+        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-white sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+          {stats.map((stat) => (
+            <div key={stat.id} className="flex flex-col gap-y-3 border-l border-white/10 pl-3">
+              <dt className="text-sm leading-6 text-slate-400">{stat.name}</dt>
+              <dd className="order-first text-3xl font-semibold tracking-tight">{stat.value}</dd>
+            </div>
+          ))}
+        </dl>
+        </div> 
+      
+      </div>
+      <div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+            <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
+              <title>App screenshot</title>
+              <defs>
+                <clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
+                  <rect width={316} height={684} rx={36} />
+                </clipPath>
+              </defs>
+              <path
+                fill="#4B5563"
+                d="M363.315 64.213C363.315 22.99 341.312 1 300.092 1H66.751C25.53 1 3.528 22.99 3.528 64.213v44.68l-.857.143A2 2 0 0 0 1 111.009v24.611a2 2 0 0 0 1.671 1.973l.95.158a2.26 2.26 0 0 1-.093.236v26.173c.212.1.398.296.541.643l-1.398.233A2 2 0 0 0 1 167.009v47.611a2 2 0 0 0 1.671 1.973l1.368.228c-.139.319-.314.533-.511.653v16.637c.221.104.414.313.56.689l-1.417.236A2 2 0 0 0 1 237.009v47.611a2 2 0 0 0 1.671 1.973l1.347.225c-.135.294-.302.493-.49.607v377.681c0 41.213 22 63.208 63.223 63.208h95.074c.947-.504 2.717-.843 4.745-.843l.141.001h.194l.086-.001 33.704.005c1.849.043 3.442.37 4.323.838h95.074c41.222 0 63.223-21.999 63.223-63.212v-394.63c-.259-.275-.48-.796-.63-1.47l-.011-.133 1.655-.276A2 2 0 0 0 366 266.62v-77.611a2 2 0 0 0-1.671-1.973l-1.712-.285c.148-.839.396-1.491.698-1.811V64.213Z"
+              />
+              <path
+                fill="#343E4E"
+                d="M16 59c0-23.748 19.252-43 43-43h246c23.748 0 43 19.252 43 43v615c0 23.196-18.804 42-42 42H58c-23.196 0-42-18.804-42-42V59Z"
+              />
+              <foreignObject
+                width={316}
+                height={684}
+                transform="translate(24 24)"
+                clipPath="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)"
+              >
+              <img src={phone} alt="" />
+              </foreignObject>
+            </svg>
+        </div>
+     </div> 
+    </div>
+          <div className="relative isolate overflow-hidden bg-slate-800 py-24 sm:py-32">
             <img
-              src={accent}
+              src={multi2}
               alt=""
               className="absolute mix-blend-multiply inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
             />
@@ -139,8 +246,8 @@ const stats = [
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-white">
-                  <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-sky-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-sky-400/20">
+                    <feature.icon className="h-6 w-6 text-sky-400" aria-hidden="true" />
                   </div>
                   {feature.name}
                 </dt>
@@ -151,90 +258,30 @@ const stats = [
         </div>
       </div>
     </div>
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-4 rounded-lg">
-            
-            <ReactCompareSlider
-              itemOne={<ReactCompareSliderImage src={day} alt="Image one" className="rounded-3xl" />}
-              itemTwo={<ReactCompareSliderImage src={darkblue} alt="Image two" className="rounded-3xl"/>}
-              
-            />
-      
-            </div>
-          
-        <div>
-            <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
-              
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Bright at Night and Invisible by Day
-              </h1>
-              <div className="max-w-xl">
-                <p className="mt-6">
-                The lights are installed underneath the house’s roofline, making them unnoticeable from the street during the day. To make them even more invisible, we offer color matching so the light’s casing will match the trim piece of your house, giving them the ultimate disguise.
-                </p>
-              </div>
-              <div className="mt-10 flex">
-              <a href="/Quote" className="text-base font-semibold leading-7 text-sky-800">
-                Request a free consultation <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-            </div>
-            </div>
-           </div>
-          </div>
-         </div>   
-    <div className="relative isolate overflow-hidden bg-gray-700 py-24 sm:py-32">
-      <img
-        src={bronco}
-        alt=""
-        className="absolute mix-blend-multiply inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      />
-      <div className="mx-auto max-w-1xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Control everything from your phone</h2>
-          <h4 className="mt-6 text-lg leading-8 text-white">
-            Use our app to change to any colors, set timers, and even create a new sequence.
-          </h4>
-        </div>
-      <div className="py-24 sm:py-28">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <dl className="grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-3">
-          {stats.map((stat) => (
-            <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-white">{stat.name}</dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">
-                {stat.value}
-              </dd>
-            </div>
-          ))}
-        </dl>
-      </div>
-    </div>
-    </div>
-    <div className="bg-white">
+    
+    <div className="bg-slate-900">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:py-40 lg:px-8">
-        <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
-          <h1 className="text-4xl font-bold leading-10 tracking-tight text-grey-900">Frequently asked questions</h1>
-          <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
+        <div className="mx-auto max-w-4xl divide-y divide-gray-100/50">
+          <h1 className="text-4xl font-bold leading-10 tracking-tight text-white">Frequently asked questions</h1>
+          <dl className="mt-10 space-y-6 divide-y divide-slate-100/10">
             {faqs.map((faq) => (
               <Disclosure as="div" key={faq.question} className="pt-6">
                 {({ open }) => (
                   <>
                     <dt>
-                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
                         <span className="text-base font-semibold leading-7">{faq.question}</span>
                         <span className="ml-6 flex h-7 items-center">
                           {open ? (
-                            <MinusSmallIcon className="h-6 w-6 text-sky-800" aria-hidden="true" />
+                            <MinusSmallIcon className="h-6 w-6 text-sky-400" aria-hidden="true" />
                           ) : (
-                            <PlusSmallIcon className="h-6 w-6 text-sky-800" aria-hidden="true" />
+                            <PlusSmallIcon className="h-6 w-6 text-sky-400" aria-hidden="true" />
                           )}
                         </span>
                       </Disclosure.Button>
                     </dt>
                     <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <p className="text-base leading-7 text-gray-600">{faq.answer}</p>
+                      <p className="text-base leading-7 text-slate-400">{faq.answer}</p>
                     </Disclosure.Panel>
                   </>
                 )}
