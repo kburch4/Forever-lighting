222db1483a60d5a8576eb211000c58af1571a4ba
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-09-20 11:48:52 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-
 import './App.css';
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import NavBar from './Layout/NavBar';
 import Home from './Layout/Home';
-import Holiday from './Layout/Pages/Holiday';
-import Gameday from './Layout/Pages/Gameday';
-import Accent from './Layout/Pages/Accent';
 import Contact from './Layout/Pages/Contact';
 import Footer from './Layout/Footer';
 import Quote from './Layout/Pages/Quote';
-import Security from './Layout/Pages/Security';
 
 function App() {
   return (
@@ -18,10 +13,6 @@ function App() {
       <NavBar />
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/Holiday" element={<Holiday />}/>
-          <Route path="/Security" element={<Security />}/>
-          <Route path="/Gameday" element={<Gameday />}/>
-          <Route path="/Accent" element={<Accent />}/>
           <Route path="/Contact" element={<Contact />}/>
           <Route path="/Quote" element={<Quote />}/>
         </Routes>
