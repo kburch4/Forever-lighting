b26ed4a8d420fb34fce4e7fa70557b91e79f5c1f
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-10-12 19:47:07 -0600

    Update Quote.js

--- src/Layout/Pages/Quote.js
+++ src/Layout/Pages/Quote.js
@@ -30,7 +30,7 @@ export default function Quote() {
   
   return (
     
-    <div className="isolate bg-slate-800 py-24 px-6 sm:py-32 lg:px-8">
+    <div className="isolate bg-sky-900/10 py-24 px-6 sm:py-32 lg:px-8">
       
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Request a Quote</h2>
@@ -71,6 +71,21 @@ export default function Quote() {
             </div>
           </div>
           
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-white">
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="email_address"
+                id="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md bg-slate-900 border-0 py-2 px-3.5 text-slate-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
           <div className="sm:col-span-2">
             <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-white">
               Phone number
