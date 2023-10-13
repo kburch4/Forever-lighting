3b50f6b9642e4a7c2218c7cb0364a6a903025561
Kody_Burch4 <101694900+kburch4@users.noreply.github.com>
2023-10-12 19:45:40 -0600

    Update Splider.js

--- src/Layout/Pages/Splider.js
+++ src/Layout/Pages/Splider.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/css';
-import accent from "../../Componets/light pictures/accent.JPG"
-import green from "../../Componets/light pictures/green.JPG"
-import usa from "../../Componets/light pictures/usa.JPG"
-import easter from "../../Componets/light pictures/easter.JPG"
-import christmas from "../../Componets/light pictures/christmas.JPG"
-import valentines from "../../Componets/light pictures/valentines.JPG"
+import accent from "../../Componets/light pictures/accent1.PNG"
+import green from "../../Componets/light pictures/green.PNG"
+import purple from "../../Componets/light pictures/purplep.PNG"
+import multi from "../../Componets/light pictures/laker.jpg"
+import christmas from "../../Componets/light pictures/white.PNG"
+import usa from "../../Componets/light pictures/usa2.jpg"
 
 export default function Splider() {
     
     return(
-        <div className="bg-slate-800">
+        <div className="bg-sky-900/10">
         <Splide aria-label="My Favorite Images" className=" md:px-32  md:space-x-10" options={ {
           perMove : 1,
           perPage : 2,
@@ -25,23 +25,23 @@ export default function Splider() {
           }
         } }>
           <SplideSlide>
-            <img src={usa} alt="1" className="h-auto w-auto shadow-xl rounded-3xl"
+            <img src={purple} alt="1" className="h-full w-full shadow-xl rounded-3xl"
             />
           </SplideSlide>
           <SplideSlide>
-            <img src={accent} alt="2" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={accent} alt="2" className="h-full w-full shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={green} alt="3" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={green} alt="3" className="h-full w-full shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={valentines} alt="4" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={usa} alt="4" className="h-full w-full shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={easter} alt="5" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={multi} alt="5" className="h-full w-full shadow-xl rounded-3xl"/>
           </SplideSlide>
           <SplideSlide>
-            <img src={christmas} alt="6" className="h-auto w-auto shadow-xl rounded-3xl"/>
+            <img src={christmas} alt="6" className="h-full w-full shadow-xl rounded-3xl"/>
           </SplideSlide>
         </Splide>
         </div>
